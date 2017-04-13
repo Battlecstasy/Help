bbdf44b97557c4cf159fe2e7ba1ace8e22068bf1
Battlecstasy <battlecstasy@gmail.com>
2017-04-13 09:29:30 -0300

    Update menu.lua

--- menu.lua
+++ menu.lua
@@ -2,11 +2,16 @@ Menu = {}
 
 function Menu:new()
   menu = {sx = 1, increase = true, r = 100, countr = 100, g = 0, countg = 0, b = 255, countb = 0}
+  -- sx é o tamanho da letra na tela, increase verifica se está aumentando, r, g e b são para as cores RGB e os countr,g,b são contadores das cores
   setmetatable(menu, {__index = Menu})
   return menu
 end
 
 function Menu:update(dt)
+  
+  --A ideia da troca de tamanho é realçar o título do jogo, que aumentando e diminuindo. O increase é basicamente um contador em boolean.
+  --Tentei dizer que menu.increase = not menu.increase e depois ~menu.increase para o primeiro if, mas não funcionou se não me engano.
+  
   if menu.sx >= 2 and menu.increase == true then
       menu.increase = false
     elseif menu.sx <= 1 and menu.increase == false then
@@ -18,6 +23,9 @@ function Menu:update(dt)
   elseif (menu.sx <= 3 and menu.increase==false) then
     menu.sx = menu.sx-1.1*dt
   end
+  
+  --Basicamente a ideia dos contadores de cor é trocar de cor, dando um efeito que realça o título do jogo.
+  --Eles começam a contar do número que recebem dentro da tabela e depois vão aumentando para quando atingirem 255, diminuirem e assim por diante.
   
   --[[if menu.r < 255 and menu.countr == 0 then
     menu.r = menu.r+50*dt
